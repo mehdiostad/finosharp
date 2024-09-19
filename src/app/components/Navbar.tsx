9ecae507c903db3c578f73fbc4f42b7239ef4196
mehdi <me.ostad80@gmail.com>
2024-09-19 19:18:04 +0330

    last update

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -2,7 +2,7 @@
 import { Button, Drawer } from "@mui/material";
 import Image from "next/image";
 import React, { useState } from "react";
-import { ModeToggle } from "./Toggle";
+import { ModeToggle } from "./ModeToggle";
 const Navbar = () => {
   const [open, setOpen] = useState(false);
 
@@ -15,22 +15,22 @@ const Navbar = () => {
       <div className="flex gap-4 items-center">
         <Image src="/Binance-Logo.png" alt="logo" width={150} height={50} />
         <ul className="lg:flex gap-4 hidden">
-          <li>Markets</li>
-          <li>Trade</li>
-          <li>Derivatives</li>
-          <li>More</li>
+          <li className="hover:text-yellow-500 transition hover:cursor-pointer font-bold">Markets</li>
+          <li className="hover:text-yellow-500 transition hover:cursor-pointer font-bold">Trade</li>
+          <li className="hover:text-yellow-500 transition hover:cursor-pointer font-bold">Derivatives</li>
+          <li className="hover:text-yellow-500 transition hover:cursor-pointer font-bold">More</li>
         </ul>
        
       </div>
-      <div className="flex gap-4 pr-8 items-center">
+      <div className="flex gap-4 pr-8 items-center justify-end">
         <ul className="flex gap-4 items-center">
-          <li>LogIn</li>
-          <li className="p-2 bg-yellow-600 rounded-lg text-white font-bold">SingUp</li>
+          <li className="hover:text-yellow-500 transition hover:cursor-pointer font-bold">LogIn</li>
+          <li className="p-2 bg-yellow-600 rounded-lg text-white font-bold hover:cursor-pointer">SingUp</li>
           <ModeToggle/>
         </ul>
 
         <Button onClick={toggleDrawer(true)} className="flex lg:hidden">
-          <Image src="/menu.png" alt="menu" width={30} height={50} className="dark:bg-white p-2 rounded-full lg:hidden flex"/>
+          <Image src="/menu.png" alt="menu" width={50} height={50} className="dark:bg-white p-2  rounded-full lg:hidden flex"/>
         </Button>
         <Drawer open={open} onClose={toggleDrawer(false)}>
           <ul className="flex flex-col gap-10 p-10">
